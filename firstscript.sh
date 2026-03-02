3a88183e0af0c569e03792f4188215fc92a5b168
dan herete egwe agha <danherete@ip-172-31-16-170.ec2.internal>
2026-03-02 20:02:39 +0000

    New file from EC2 instacne pushed for practice status

---
+++ firstscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#To run this script user most have appropriate age,password, Then will ask for a name read that name 
+
+#and welcome the user if all conditions have been meet as pre-defined
+
+echo "Welcome to the server what is your name?"
+
+read name
+
+" echo "Welcome what is your password?"
+
+
